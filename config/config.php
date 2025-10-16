d48a79f844afa7b392410ef91cfe8c2a56f35159
robertobcv <90868053+robertoBCV@users.noreply.github.com>
2025-10-15 23:32:59 -0500

    sistema hola mundo con crud y bd, final

--- config/config.php
+++ config/config.php
@@ -1,9 +1,10 @@
 <?php
 // config/config.php
-define('DB_HOST', '127.0.0.1');
+define('DB_HOST', 'localhost');
 define('DB_NAME', 'hola_mundo');
 define('DB_USER', 'root');
 define('DB_PASS', '');
+define('DB_PORT', '3306');
 define('BASE_URL', '/hola-mundo-mvc/public');
 define('UPLOAD_DIR', __DIR__ . '/../public/imagenes');
 define('MAX_IMAGE_BYTES', 2 * 1024 * 1024);
